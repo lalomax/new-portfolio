09c86a01679b1f21b586c9959ba7f90321b80265
lalomax365 <lalomax365@gmail.com>
2024-06-18 19:16:59 +0000

    feat: automatic current year on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className="py-4 text-center bg-primaryDark text-white "> &copy; 2024 lalomax all rights reserved</div>;
+  return <div className="py-4 text-center bg-primaryDark text-white "> &copy; {new Date().getFullYear()} lalomax all rights reserved</div>;
 };
 
 export default Footer;
