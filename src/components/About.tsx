4e0a9015e1c142f9fea67d6ab74d912dfe321f86
lalomax365 <lalomax365@gmail.com>
2024-08-02 20:30:30 +0000

    style: adjust subtitle bottom line

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
     <section className="dark:bg-primaryDark bg-primaryLight px-5 py-32 " id="about">
       <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between ">
         <div className="about-info ">
-          <h2 className="text-4xl  font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
+          <h2 className="text-4xl inline-block font-bold mb-5 border-b-[5px] border-indigo-600 pb-2">
             About Me
           </h2>
 
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -55,7 +55,7 @@ const Projects = () => {
     <section className="bg-primaryLight dark:bg-primaryDark px-5 py-32" id="projects">
       <div className="container mx-auto grid md:grid-cols-2 items-center md:justify-between">
         <div className="about-info mb-5 ">
-          <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
+          <h2 className="text-4xl inline-block font-bold mb-5 border-b-[5px] border-indigo-600 pb-2">
             Projects
           </h2>
 
